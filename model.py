0032a53e2a12ceb8d058105b3680a7d68cddfe6e
Ankush Tale <37273059+ankushtale@users.noreply.github.com>
2020-02-17 00:36:10 -0700

    +plotting, genetic operators, refactoring

--- model.py
+++ model.py
@@ -1,28 +1,11 @@
-"""
-Reference paper: http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.40.3735&rep=rep1&type=pdf
-https://github.com/handcraftsman/GeneticAlgorithmsWithPython
-"""
-
 import utils
-import os
-from random import random
+import numpy as np
+import pandas as pd
+import logging
+import random
+import copy
 
-
-class Country:
-    def __init__(self, t=0, s=0, m=0):
-        self.t = t
-        self.s = s
-        self.m = m
-
-
-class RateConstants:
-    def __init__(self, k_11=0, k_12=0, k_13=0, k_22=0, k_23=0, k_33=0):
-        self.k_11 = k_11
-        self.k_12 = k_12
-        self.k_13 = k_13
-        self.k_22 = k_22
-        self.k_23 = k_23
-        self.k_33 = k_33
+LIFECYCLE_LENGTH = 50
 
 
 class Log:
@@ -30,61 +13,175 @@ class Log:
         self.threat_history = {}
 
 
-def creator(parameters=None, init_threat=None):
-    if init_threat is None:
-        init_threat = [0, 0, 0]
-    if parameters is None:
-        parameters = [0.5, 0.2, 0.6, 0.5, 0.4, 0.6, 0.1, 0.1, 0.2, 0.2, 0.2, 0.2]
+def generation_lifecycle(chromosomes, init_threat):
+    """
+    Takes a population through it's lifecycle following Richardson's equation
+    :param chromosomes: initial population parameters
+    :param init_threat: initial Threat values
+    :return: Threat history values and calculated fitness
+    """
+    if not init_threat:
+        chromosomes['x_t'], chromosomes['y_t'], chromosomes['z_t'] = 0, 0, 0
 
-    #print("Parameters: ", parameters)
-    x = Country(init_threat[0], parameters[0], parameters[1])
-    y = Country(init_threat[1], parameters[2], parameters[3])
-    z = Country(init_threat[2], parameters[4], parameters[5])
-    k = RateConstants(parameters[6], parameters[7], parameters[8], parameters[9], parameters[10], parameters[11])
-    t_history = dict()
-    t_history['x'] = []
-    t_history['y'] = []
-    t_history['z'] = []
+    t_history_list = []
 
-    for i in range(50):
-        x_t, y_t, z_t = x.t, y.t, z.t
-        # print(f"Iteration: {i} - {x.t, y.t, z.t}")
-        t_history['x'].append(x_t)
-        t_history['y'].append(y_t)
-        t_history['z'].append(z_t)
+    for _ in range(LIFECYCLE_LENGTH):
+        chromosomes['x_t'] = chromosomes['x_t'] + (chromosomes['k11'] * (chromosomes['x_s'] - chromosomes['x_t'])
+                                                   + chromosomes['k23']*(chromosomes['y_t'] + chromosomes['z_t'])
+                                                   )*(chromosomes['x_m'] - chromosomes['x_t'])
 
-        x.t = x_t + (k.k_11 * (x.s - x_t) + k.k_23 * (y_t + z_t)) * (x.m - x_t)
-        y.t = y_t + (k.k_22 * (y.s - y_t) + k.k_13 * (x_t - z_t)) * (y.m - y_t)
-        z.t = z_t + (k.k_33 * (z.s - z_t) + k.k_12 * (x_t - y_t)) * (z.m - z_t)
+        chromosomes['y_t'] = chromosomes['y_t'] + (chromosomes['k22'] * (chromosomes['y_s'] - chromosomes['y_t'])
+                                                   + chromosomes['k13']*(chromosomes['x_t'] - chromosomes['z_t'])
+                                                   )*(chromosomes['y_m'] - chromosomes['y_t'])
+        chromosomes['z_t'] = chromosomes['z_t'] + (chromosomes['k33'] * (chromosomes['z_s'] - chromosomes['z_t'])
+                                                   + chromosomes['k12']*(chromosomes['x_t'] - chromosomes['y_t'])
+                                                   )*(chromosomes['z_m'] - chromosomes['z_t'])
 
-    fitness = x.t - (y.t + z.t)
-    #print("Fitness: ", fitness)
-    # Plot individual iteration history
-    # utils.plot_country_fitnesses(t_history)
+        # alliance shift check
+        for i in range(chromosomes.shape[0]):
+            if chromosomes.at[i, 'x_t'] < chromosomes.at[i, 'y_t']:
+                chromosomes.at[i, 'x_t'], chromosomes.at[i, 'y_t'] = \
+                    chromosomes.at[i, 'y_t'], chromosomes.at[i, 'x_t']
+                chromosomes.at[i, 'x_s'], chromosomes.at[i, 'y_s'] = \
+                    chromosomes.at[i, 'y_s'], chromosomes.at[i, 'x_s']
+                chromosomes.at[i, 'x_m'], chromosomes.at[i, 'y_m'] = \
+                    chromosomes.at[i, 'y_m'], chromosomes.at[i, 'x_m']
+                chromosomes.at[i, 'k11'], chromosomes.at[i, 'k22'] = \
+                    chromosomes.at[i, 'k22'], chromosomes.at[i, 'k11']
+                chromosomes.at[i, 'k23'], chromosomes.at[i, 'k13'] = \
+                    chromosomes.at[i, 'k13'], chromosomes.at[i, 'k23']
 
-    return t_history, fitness
+            if chromosomes.at[i, 'x_t'] < chromosomes.at[i, 'z_t']:
+                chromosomes.at[i, 'x_t'], chromosomes.at[i, 'z_t'] = \
+                    chromosomes.at[i, 'z_t'], chromosomes.at[i, 'x_t']
+                chromosomes.at[i, 'x_s'], chromosomes.at[i, 'z_s'] = \
+                    chromosomes.at[i, 'z_s'], chromosomes.at[i, 'x_s']
+                chromosomes.at[i, 'x_m'], chromosomes.at[i, 'z_m'] = \
+                    chromosomes.at[i, 'z_m'], chromosomes.at[i, 'x_m']
+                chromosomes.at[i, 'k11'], chromosomes.at[i, 'k33'] = \
+                    chromosomes.at[i, 'k33'], chromosomes.at[i, 'k11']
+                chromosomes.at[i, 'k23'], chromosomes.at[i, 'k12'] = \
+                    chromosomes.at[i, 'k12'], chromosomes.at[i, 'k23']
+
+        t_history_list.append(list(chromosomes['x_t']))
+        t_history_list.append(list(chromosomes['y_t']))
+        t_history_list.append(list(chromosomes['z_t']))
+
+    fitnesses = abs(chromosomes['x_t'] - (chromosomes['y_t'] + chromosomes['z_t']))
+
+    timeline_range = np.array(list(range(1,LIFECYCLE_LENGTH+1))*3)
+    timeline_range.sort()
+
+    countries_range = np.array(['x', 'y', 'z']*LIFECYCLE_LENGTH)
+    t_history_list = np.array(t_history_list)
+    t_history = pd.DataFrame(data=t_history_list.T, columns=pd.MultiIndex.from_tuples(zip(timeline_range, countries_range)))
+
+    return t_history, fitnesses
 
 
-def main(n_generations):
+def selection(chromosomes, fitnesses, method):
+    """
+    Implements selection amongst generations
+    :param chromosomes: population
+    :param fitnesses: fitness values
+    :param method: Choice between different selection strategies
+    :return: new population
+    """
+
+    if method is None:
+        method = 'fitness_proportionate'
+
+    dict_fitnesses = fitnesses.to_dict()
+    n_population = chromosomes.shape[0]
+    selected_individuals = pd.DataFrame(data=None, columns=chromosomes.columns)
+
+    # Or use df.sample(weights='a')
+    if method == 'fitness_proportionate':
+        for _ in range(n_population):
+            loc = utils.weighted_random_choice(dict_fitnesses)
+            selected_individuals = selected_individuals.append(chromosomes.iloc[loc])
+    else:
+        selected_individuals = chromosomes.copy()
+    
+    return selected_individuals.reset_index(drop=True)
+
+
+def mutate(chromosomes, prob):
+    """
+    Implements mutation amongst generations
+    :param chromosomes: population
+    :param prob: mutation probability p_m
+    :return: mutated population
+    """
+    idx_to_mutate = list(chromosomes.sample(frac=prob, replace=True, random_state=1).index)
+
+    columns = list(chromosomes.columns)
+    len_cols = chromosomes.shape[1]-1
+
+    for id in idx_to_mutate:
+        chromosomes.at[id, columns[random.randint(1, len_cols)]] = random.random()
+
+    return chromosomes
+
+
+def crossover(chromosomes, prob):
+    """
+    Implements single point crossover
+    :param chromosomes: chromosomes
+    :param prob: probability of crossover
+    :return: new population
+    """
+    idx_to_crossover = list(chromosomes.sample(frac=prob, replace=True).index)
+    len_cols = chromosomes.shape[1] - 1
+
+    for i in range(0, len(idx_to_crossover), 2):
+        crossover_point = random.randint(1, len_cols)
+        temp_i = copy.deepcopy(chromosomes.iloc[idx_to_crossover[i]][:crossover_point])
+        temp_i1 = copy.deepcopy(chromosomes.iloc[idx_to_crossover[i+1]][:crossover_point])
+
+        chromosomes.iloc[idx_to_crossover[i]][:crossover_point] = temp_i1
+        chromosomes.iloc[idx_to_crossover[i+1]][:crossover_point] = temp_i
+
+    return chromosomes
+
+
+def main(n_generations, n_population):
     generations = range(n_generations)
+    generation = 1
+    curr_fitnesses_sum = 100
+    prev_fitnesses_sum = 0
     log = Log()
+    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M %p',
+                        level=logging.DEBUG)
 
-    for generation in generations:
-        parameters = [round(random(), 4), round(random(), 4), round(random(), 4), round(random(), 4),
-                      round(random(), 4), round(random(), 4), round(random(), 4), round(random(), 4),
-                      round(random(), 4), round(random(), 4), round(random(), 4), round(random(), 4)]
-        t_history, fitness = creator(parameters= parameters)
+    # Initialize population
+    chromosomes = pd.DataFrame(np.random.random(size=(n_population, 12)),
+                               columns=['x_s', 'x_m', 'y_s', 'y_m', 'z_s', 'z_m',
+                                        'k11', 'k22', 'k33', 'k12', 'k13', 'k23'])
+    fitnesses = pd.Series(np.linspace(3, 33, 3))
 
-        log.threat_history[generation] = {"parameters": parameters,
-                                          "fitness": fitness,
-                                          "threats": t_history}
+    while abs(curr_fitnesses_sum - prev_fitnesses_sum) > 0.01:
 
-        print("Generation {} has fitness {}:".format(generation, fitness))
+        prev_fitnesses_sum = sum(fitnesses)
 
-    for generation, log in log.threat_history.items():
-        if log["fitness"] > 0:
-            utils.plot_country_fitnesses(generation, log["threats"], save_figure=True)
+        t_history, fitnesses = generation_lifecycle(chromosomes=chromosomes, init_threat=False)
+
+        new_gen_chromosomes = selection(chromosomes=chromosomes, fitnesses=fitnesses, method="fitness_proportionate")
+
+        new_gen_chromosomes = mutate(chromosomes=new_gen_chromosomes, prob=0.6)
+
+        new_gen_chromosomes = crossover(chromosomes=new_gen_chromosomes, prob=0.4)
+
+        chromosomes = new_gen_chromosomes
+        curr_fitnesses_sum = sum(fitnesses)
+        logging.debug("Generation %d: #Uniques %d, sumdiff: %.6f", generation, len(fitnesses.unique()),
+                      abs(curr_fitnesses_sum - prev_fitnesses_sum))
+        generation += 1
+
+    # Plotting
+    utils.plot_country_threats_over_time(generation, t_history, fitnesses, save_figure=True)
+    utils.plot_fitnesses(fitnesses, save_figure=True)
 
 
 if __name__ == '__main__':
-    main(n_generations=50)
+    main(n_generations=50, n_population=100)
--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 from matplotlib import pyplot as plt
 from datetime import datetime
 import os
+import numpy.random as npr
 
 
-def plot_country_fitnesses(generation, threats, save_figure):
+def plot_country_fitness(generation, threats, save_figure):
     plt.plot(range(1, len(threats['x']) + 1), threats['x'], color="blue", label="Country X")
     plt.plot(range(1, len(threats['y']) + 1), threats['y'], color="green", label="Country Y")
     plt.plot(range(1, len(threats['z']) + 1), threats['z'], color="red", label="Country Z")
-    plt.title("Generation: {} threats for countries X, Y, Z".format(generation))
+    plt.title("Individual: {} threats for countries X, Y, Z".format(generation))
     plt.xlabel("time")
     plt.ylabel("threat potential")
     plt.legend(loc="best")
@@ -19,3 +20,60 @@ def plot_country_fitnesses(generation, threats, save_figure):
         plt.close()
     else:
         plt.show()
+
+
+def plot_country_threats_over_time(generation, threats, fitnesses, save_figure):
+
+    n_population = threats.shape[0]
+
+    for individual in range(n_population):
+        i_threats = list(threats.iloc[individual])
+        x_threats, y_threats, z_threats = [], [], []
+        fitness = fitnesses.iloc[individual]
+
+        for i in range(0, len(i_threats), 3):
+            x_threats.append(i_threats[i])
+            y_threats.append(i_threats[i+1])
+            z_threats.append(i_threats[i+2])
+
+        plt.plot(range(1, len(x_threats) + 1), x_threats, color="blue", label="Country X")
+        plt.plot(range(1, len(y_threats) + 1), y_threats, color="green", label="Country Y")
+        plt.plot(range(1, len(z_threats) + 1), z_threats, color="red", label="Country Z")
+        plt.title("Generation : {} Individual: {} Fitness: {}".format(generation, individual, fitness))
+        plt.xlabel("time")
+        plt.ylabel("threat potential")
+        plt.legend(loc="best")
+        if save_figure:
+            folder = os.path.join("./plots", datetime.now().strftime('%Y-%m-%d_%H-%M') + "_Gen_" + str(generation))
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+            plt.savefig("{}/individual_{}_fitness_{}_threats_over_time.png".format(folder, individual, fitness))
+            plt.close()
+        else:
+            plt.show()
+
+
+def plot_fitnesses(fitnesses, save_figure):
+    l_fitness = list(fitnesses)
+
+    plt.plot(range(0, len(l_fitness)), l_fitness, color="green")
+    plt.title("Fitnesses for all individual")
+    plt.xlabel("individuals")
+    plt.ylabel("fitness values")
+
+    if save_figure:
+        folder = os.path.join("./plots", datetime.now().strftime('%Y-%m-%d_%H-%M') + "_Fitness")
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        plt.savefig("{}/all_fitnesses.png".format(folder))
+        plt.close()
+    else:
+        plt.show()
+
+
+def weighted_random_choice(choices):
+    population = choices.values()
+    max = sum([1/x for x in population if str(x) != 'nan'])
+    selection_probs = [(1/(c*max)) for c in population if str(c) != 'nan']
+
+    return npr.choice(len(population), p=selection_probs)
